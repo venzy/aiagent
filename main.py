4ba89f256d4f4994173bd39510db474397eea608
venzy <david.venz@gmail.com>
2025-06-19 09:38:33 +1000

    Ch3 L2 Function Declaration

--- main.py
+++ main.py
@@ -36,15 +36,49 @@ def main():
     ]
 
     # System prompt (hard coded for now)
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+The root directory is the working directory, and is represented by a dot (.) in your function calls.
+"""
+
+    # Declare our functions
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     # Generate content using the specified prompt
     response = client.models.generate_content(
         model=model,
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
         )
-    print(response.text)
+    
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+    #print(response.text)
 
     # Metrics - may be used for more than verbose output
     prompt_tokens = response.usage_metadata.prompt_token_count
